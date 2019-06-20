e410a5dc0a9d1359418be474925b5b7cfa3ced7e
yusufajiib <51849143+yusufajiib@users.noreply.github.com>
2019-06-20 09:13:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -32,13 +32,11 @@
     $db = "<Submission-1-db>";
 
     try {
-    $conn = new PDO("sqlsrv:server = tcp:ssubmission-appserver.database.windows.net,1433; Database = Submission-1-db", "Yusufajiib", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
 
     if (isset($_POST['submit'])) {
         try {

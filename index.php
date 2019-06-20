a84250dae840f33e9460f8f2c22166c9b2193968
yusufajiib <51849143+yusufajiib@users.noreply.github.com>
2019-06-20 09:07:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
  <?php
     $host = "<ssubmission-appserver.database.windows.net>";
     $user = "<Yusufajiib>";
-    $pass = "<1bnurohmaT>";
+    $pass = "<>";
     $db = "<Submission-1-db>";
 
     try {
